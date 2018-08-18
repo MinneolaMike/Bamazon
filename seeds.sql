e944125c28ebdf38c7a447ec10d9ad6e68d4aa1f
chefgonzo <chefgonzo@gmail.com>
2018-08-18 13:15:04 -0400

    seeds.sql completed and database updated with values

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+-- Populate products database with around 10 different products --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Napkins", "Household", 4.99, 400), ("Paper Towels", "Household", 7.65, 250), ("Men's Shirts", "Clothing", 39.84, 70), ("Women's Shirts", "Clothing", 36.52, 65), ("Tablet", "Electronics", 99.89, 50), ("Computer", "Electronics", 750.00, 20), ("Phone", "Electronics", 695.36, 110), ("Bleach", "Household", 1.99, 350), ("Stereo", "Electronics", 299.99, 85), ("Cheap Car", "Vehicles", 13599.99, 20), ("Luxury Car", "Vehicles", 39965.84, 15), ("Sports Car", "Vehicles", 102641.61, 400);
+
+
+
+
+
+
+
+
